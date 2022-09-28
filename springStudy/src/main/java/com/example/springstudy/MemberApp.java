5962d2b0d944ce366fbc04f87ee2b78ca505b9e4
gktjdfhr2 <74586766+gktjdfhr2@users.noreply.github.com>
2022-09-29 00:09:06 +0900

    Feat: AppConfig Spring적용

--- springStudy/src/main/java/com/example/springstudy/MemberApp.java
+++ springStudy/src/main/java/com/example/springstudy/MemberApp.java
@@ -5,11 +5,19 @@ import com.example.springstudy.eNum.Grade;
 import com.example.springstudy.member.Member;
 import com.example.springstudy.service.member.MemberService;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+
         Member member = new  Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- springStudy/src/main/java/com/example/springstudy/config/AppConfig.java
+++ springStudy/src/main/java/com/example/springstudy/config/AppConfig.java
@@ -1,21 +1,37 @@
 package com.example.springstudy.config;
 
+import com.example.springstudy.discount.DiscountPolicy;
 import com.example.springstudy.discount.FixDiscountPolicy;
+import com.example.springstudy.discount.RateDiscountPolicy;
+import com.example.springstudy.repository.MemberRepository;
 import com.example.springstudy.repository.MemoryMemberRepository;
 import com.example.springstudy.service.member.MemberService;
 import com.example.springstudy.service.member.MemberServiceImpl;
 import com.example.springstudy.service.order.OrderService;
 import com.example.springstudy.service.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 
