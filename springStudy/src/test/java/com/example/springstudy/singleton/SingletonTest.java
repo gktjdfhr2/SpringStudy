f346f39b486ab9a75b17fc9cb417b75ab1606f8a
gktjdfhr2 <74586766+gktjdfhr2@users.noreply.github.com>
2022-12-29 23:19:09 +0900

    Feat: 싱글톤 컨테이너

---
+++ springStudy/src/test/java/com/example/springstudy/singleton/SingletonTest.java
@@ -0,0 +1,72 @@
+package com.example.springstudy.singleton;
+
+import com.example.springstudy.config.AppConfig;
+import com.example.springstudy.service.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 =" + memberService1);
+        System.out.println("memberService2 =" + memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+    }
+
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+        //2. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        //참조값이 같은 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " +singletonService2);
+
+        //참조값이 다르면 에러 발생
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void springContainer() {
+
+        //AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회: 호출할 때 마다 스프링 빈에서 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2. 조회: 호출할 때 마다 스프링 빈에서 객체를 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 같은 것을 확인
+        System.out.println("memberService1 =" + memberService1);
+        System.out.println("memberService2 =" + memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+
+    }
+
+}
