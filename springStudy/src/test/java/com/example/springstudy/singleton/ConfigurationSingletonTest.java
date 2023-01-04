7da42bd21a9818764756cdaa9698626d2c587676
하성록 <gktjdfhr2@naver.com>
2023-01-04 21:24:39 +0900

    Feat: 싱글톤 컨테이너

---
+++ springStudy/src/test/java/com/example/springstudy/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,40 @@
+package com.example.springstudy.singleton;
+
+import com.example.springstudy.AppConfig;
+import com.example.springstudy.repository.MemberRepository;
+import com.example.springstudy.service.member.MemberServiceImpl;
+import com.example.springstudy.service.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository" + memberRepository1);
+        System.out.println("orderService -> memberRepository" + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository1);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository2);
+    }
+
+    @Test
+    void configuration() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean =" + bean.getClass());
+    }
+}
